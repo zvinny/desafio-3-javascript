39274a2e82f36b08906917409f6005fd2341c1a0
Vinícius <vinicius.ss@outlook.com.br>
2026-05-15 17:00:18 -0300

    feat: adiciona sistema de herois com classes

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Projeto iniciado com sucesso!");
+class heroi {
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+    atacar(){
+        let ataque;
+         if(this.tipo == "mago"){
+            ataque = "magia;"
+         }
+         else if(this.tipo == "guerreiro"){
+            ataque = "espada"
+         }
+         else if(this.tipo == "monge"){
+            ataque = "artes marciais"
+         }
+         else{
+            ataque = "shuriken"
+         }
+         console.log(`O ${this.tipo} atacou usando ${ataque}`)
+    }
+}
+
+const novoHeroi = new heroi ("Vinicius", 29,"ninja")
+
+novoHeroi.atacar()
